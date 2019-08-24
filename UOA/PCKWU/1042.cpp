e63d5edebbd17865a161a7f49f68e5c24a0c9831
Natsuki Ogata <natukin1978@gmail.com>
2019-08-24 21:12:08 +0900

    Problem P02: Yes, I have a number

---
+++ UOA/PCKWU/1042.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename T>
+using vec1 = vector<T>;
+
+constexpr auto cin2line([] {
+    string s;
+    getline(cin, s);
+    return s;
+});
+
+vec1<string> Split(const string& str, char sep)
+{
+    vec1<string> v;
+    stringstream ss(str + sep);
+    string       line;
+    while (getline(ss, line, sep)) {
+        v.push_back(line);
+    }
+    return v;
+}
+
+int main()
+{
+    while (true) {
+        const string S(cin2line());
+        if (S == "END OF INPUT") break;
+        const auto strs(Split(S, ' '));
+        for (const auto& str : strs) {
+            cout << str.size();
+        }
+        cout << endl;
+    }
+    return 0;
+}
