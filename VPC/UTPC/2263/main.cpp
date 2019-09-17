e5d6066027b9233032a55c0b5fd88ff8dc387611
Natsuki Ogata <natukin1978@gmail.com>
2019-09-17 11:10:26 +0900

    問題 E : ファーストアクセプタンス (Not clear)
    test failed: 36 AC / 37 cases

--- VPC/UTPC/2263/main.cpp
+++ VPC/UTPC/2263/main.cpp
@@ -23,29 +23,49 @@ vec1<T> cin2vec(size_t size)
     return vec1;
 }
 
+using AB = tuple<int64_t, int64_t>;
+using BA = tuple<int64_t, int64_t>;
+
 void sub()
 {
     const auto N(cin2var<size_t>());
 
-    using BA = tuple<int64_t, int64_t>;
+    vec1<AB> abs;
     vec1<BA> bas;
     for (size_t i = 0; i < N; ++i) {
         const auto ab(cin2vec<int64_t>(2));
         if (ab[0] > ab[1]) continue;  // 絶対に負ける
+        abs.push_back(make_tuple(ab[0], ab[1]));
         bas.push_back(make_tuple(ab[1], ab[0]));
     }
 
+    sort(abs.begin(), abs.end());
     sort(bas.begin(), bas.end());
 
-    int64_t time = 0;
-    int64_t win  = 0;
-    for (const auto& ba : bas) {
-        if (time + get<1>(ba) <= get<0>(ba)) {
-            time += get<1>(ba);
-            ++win;
+    // プランA
+    int64_t winA = 0;
+    {
+        int64_t time = 0;
+        for (const auto& ab : abs) {
+            if (time + get<0>(ab) <= get<1>(ab)) {
+                time += get<0>(ab);
+                ++winA;
+            }
         }
     }
-    cout << win << endl;
+    // プランB
+    int64_t winB = 0;
+    {
+        int64_t time = 0;
+        for (const auto& ba : bas) {
+            if (time + get<1>(ba) <= get<0>(ba)) {
+                time += get<1>(ba);
+                ++winB;
+            }
+        }
+    }
+
+    cout << max(winA, winB) << endl;
 }
 
 int main()
