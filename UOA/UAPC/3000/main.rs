455c5dd5a4e5df80c00a0a22f9747f97069dafbc
Natsuki Ogata <natukin1978@gmail.com>
2019-11-26 11:20:49 +0900

    仮

---
+++ UOA/UAPC/3000/main.rs
@@ -0,0 +1,89 @@
+#[allow(dead_code)]
+fn read_line(n: usize) -> String {
+    let mut ss = String::new();
+    for _i in 0..n {
+        let mut s = String::new();
+        std::io::stdin().read_line(&mut s).unwrap();
+        ss.push_str(&s);
+    }
+    ss
+}
+
+macro_rules! parse {
+    ($s:expr => $($r:tt)*) => {
+        let s = $s;
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        use std::io::Read;
+        let mut s = String::new();
+        std::io::stdin().read_to_string(&mut s).unwrap();
+        parse!{s => $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+    ($iter:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main()
+{
+    parse!{
+        immut_ss: [i32; 2],
+    }
+    let mut ss = immut_ss;
+    let diff = (ss[0] - ss[1]).abs();
+    if diff > 180 {
+        if ss[0] > ss[1] {
+            ss[0] -= 360;
+        }
+        else {
+            ss[1] -= 360;
+        }
+    }
+    let mut over180_count = 0;
+    for s in &ss {
+        if *s > 180 {
+            over180_count += 1;
+        }
+    }
+    println!("{:?}", ss);
+    let mut sum;
+    if over180_count != 1 {
+        sum = ss[0] + ss[1];
+    }
+    else {
+        sum = ss[0] - ss[1];
+    }
+    sum = sum.abs();
+    let ans = sum / 2;
+    println!("{}", ans);
+}
