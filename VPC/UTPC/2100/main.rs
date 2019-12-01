cae85633c1197cc9afa353fa6282f32cf9cea78a
Natsuki Ogata <natukin1978@gmail.com>
2019-12-01 12:59:34 +0900

    VPC - UTPC  2100 Problem A:サイゾウ (Clear)

---
+++ VPC/UTPC/2100/main.rs
@@ -0,0 +1,101 @@
+// VPC - UTPC  2100
+// Problem A:サイゾウ
+
+#[allow(dead_code)]
+fn read_line(n: usize) -> String {
+    let mut ss = String::new();
+    for _i in 0..n {
+        let mut s = String::new();
+        std::io::stdin().read_line(&mut s).unwrap();
+        ss.push_str(&s);
+    }
+    ss
+}
+
+macro_rules! parse {
+    ($s:expr => $($r:tt)*) => {
+        let s = $s;
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        use std::io::Read;
+        let mut s = String::new();
+        std::io::stdin().read_to_string(&mut s).unwrap();
+        parse!{s => $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+    ($iter:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn sub()
+{
+    parse!{
+        read_line(2) =>
+        n: usize,
+        blocks: [i32; n],
+    }
+    let mut prev = -1;
+    let mut up   = 0;
+    let mut down = 0;
+    for block in blocks {
+        if prev < 0 {
+            prev = block;
+            continue;
+        }
+        if prev < block {
+            let diff = block - prev;
+            if up < diff {
+                up = diff;
+            }
+        }
+        else if prev > block {
+            let diff = prev - block;
+            if down < diff {
+                down = diff;
+            }
+        }
+        prev = block;
+    }
+    println!("{} {}", up, down);
+}
+
+fn main()
+{
+    parse!{
+        read_line(1) =>
+        t: usize,
+    }
+    for _i in 0..t {
+        sub();
+    }
+}
