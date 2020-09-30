bd2af3d7626a2a2a4571e64bd7d9b44b18aa5457
Natsuki Ogata <natukin1978@gmail.com>
2020-09-30 14:40:40 +0900

    JOI - Prelim  0584 (Not clear)

---
+++ JOI/Prelim/0584/base.rs
@@ -0,0 +1,88 @@
+// JOI - Prelim  0584
+
+#[allow(dead_code)]
+fn read_line(n: usize) -> String {
+    let mut ss = String::new();
+    for _i in 0..n {
+        let mut s = String::new();
+        std::io::stdin().read_line(&mut s).unwrap();
+        ss.push_str(&s);
+    }
+    ss
+}
+
+macro_rules! parse {
+    ($s:expr => $($r:tt)*) => {
+        let s = $s;
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        use std::io::Read;
+        let mut s = String::new();
+        std::io::stdin().read_to_string(&mut s).unwrap();
+        parse!{s => $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+    ($iter:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main()
+{
+    parse!{
+        n: usize,
+        vs: [i32; n],
+    }
+
+    let mut vs = vs.to_vec();
+    vs.sort();
+    let mut top3 = Vec::with_capacity(3);
+    for i in 0..3 {
+        top3.push(vs[i]);
+    }
+
+    let mut list = Vec::with_capacity(9);
+    for i in 0..3 {
+        for j in 0..3 {
+            if i == j {
+                continue;
+            }
+            let m_s = top3[i].to_string() + &top3[j].to_string();
+            let m_n: i32 = m_s.parse().unwrap();
+            list.push(m_n);
+        }
+    }
+
+    list.sort();
+
+    println!("{}", list[2]);
+}
