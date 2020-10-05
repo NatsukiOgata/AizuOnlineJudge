2aede761cf80b9ec6f2b2f76ad1d738b38db01be
Natsuki Ogata <natukin1978@gmail.com>
2020-10-05 14:34:37 +0900

    VPC - RUPC  2938 (Clear)

---
+++ VPC/RUPC/2938/main.rs
@@ -0,0 +1,82 @@
+// VPC - RUPC  2938
+
+#[allow(dead_code)]
+fn read_line(n: usize) -> String {
+    let mut ss = String::new();
+    for _i in 0..n {
+        let mut s = String::new();
+        std::io::stdin().read_line(&mut s).unwrap();
+        ss.push_str(&s);
+    }
+    ss
+}
+
+macro_rules! parse {
+    ($s:expr => $($r:tt)*) => {
+        let s = $s;
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        use std::io::Read;
+        let mut s = String::new();
+        std::io::stdin().read_to_string(&mut s).unwrap();
+        parse!{s => $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+    ($iter:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main()
+{
+    parse!{
+        n: i32,
+        ps: [char; n],
+    }
+
+    if *ps.last().unwrap() == 'T' {
+        // 末尾がTなら走査不要
+        println!("T");
+        return;
+    }
+
+    let mut ans = ps[0];
+    for i in 1..ps.len() {
+        if ans == 'T' && ps[i] == 'F' {
+            ans = 'F';
+        }
+        else {
+            ans = 'T';
+        }
+    }
+    println!("{}", ans);
+}
