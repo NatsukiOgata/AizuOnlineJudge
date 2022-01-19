fa5d3cf93f5d8ce0cd78f81fe77692d11120a12f
Natsuki Ogata <natukin1978@gmail.com>
2022-01-19 12:17:16 +0900

    JOI - Prelim - 0577 (Clear)

---
+++ JOI/Prelim/0577/main.rs
@@ -0,0 +1,86 @@
+// JOI - Prelim - 0577
+
+use std::collections::*;
+
+#[allow(dead_code)]
+fn read_line(n: usize) -> String {
+    let mut ss = String::new();
+    for _i in 0..n {
+        let mut s = String::new();
+        std::io::stdin().read_line(&mut s).unwrap();
+        ss.push_str(&s);
+    }
+    ss
+}
+
+macro_rules! parse {
+    ($s:expr => $($r:tt)*) => {
+        let s = $s;
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        use std::io::Read;
+        let mut s = String::new();
+        std::io::stdin().read_to_string(&mut s).unwrap();
+        parse!{s => $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+    ($iter:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    let cnt = 3;
+    parse! {
+        n: usize,
+        players: [[isize; cnt]; n],
+    }
+    let mut points = vec![0; n];
+    for t in 0..cnt {
+        let mut point_cnt = HashMap::new();
+        for player in &players {
+            let no = player[t];
+            *point_cnt.entry(no).or_insert(0) += 1;
+        }
+        let mut i = 0;
+        for player in &players {
+            let no = player[t];
+            if *point_cnt.get(&no).unwrap() < 2 {
+                points[i] += no;
+            }
+            i += 1;
+        }
+    }
+    for point in points {
+        println!("{}", point);
+    }
+}
