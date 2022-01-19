e141fb5977acc91f8e53e846f8ffb23644677d3f
Natsuki Ogata <natukin1978@gmail.com>
2022-01-19 09:53:15 +0900

    JOI - Prelim - 0576 (Clear)

---
+++ JOI/Prelim/0576/main.rs
@@ -0,0 +1,72 @@
+// JOI - Prelim - 0576
+
+#[allow(dead_code)]
+fn read_line(n: usize) -> String {
+    let mut ss = String::new();
+    for _i in 0..n {
+        let mut s = String::new();
+        std::io::stdin().read_line(&mut s).unwrap();
+        ss.push_str(&s);
+    }
+    ss
+}
+
+macro_rules! parse {
+    ($s:expr => $($r:tt)*) => {
+        let s = $s;
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        use std::io::Read;
+        let mut s = String::new();
+        std::io::stdin().read_to_string(&mut s).unwrap();
+        parse!{s => $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+    ($iter:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    parse! {
+        l: usize,
+        a: usize,
+        b: usize,
+        c: usize,
+        d: usize,
+    }
+    let n = a / c + if a % c == 0 { 0 } else { 1 };
+    let m = b / d + if b % d == 0 { 0 } else { 1 };
+    let max = std::cmp::max(n, m);
+    let ans = l - max;
+    println!("{}", ans);
+}
