fc1a0da032ea7a0cc11a163f5b1c587b11bdfd1d
Natsuki Ogata <natukin1978@gmail.com>
2022-01-21 11:31:05 +0900

    JOI - Prelim - 0686 (Clear)

---
+++ JOI/Prelim/0686/main.rs
@@ -0,0 +1,84 @@
+// JOI - Prelim - 0686
+
+#[allow(dead_code)]
+fn read_line(n: usize) -> String {
+    let mut ss = String::new();
+    for _i in 0..n {
+        let mut s = String::new();
+        std::io::stdin().read_line(&mut s).unwrap();
+        ss.push_str(&s);
+    }
+    ss
+}
+
+macro_rules! parse {
+    ($s:expr => $($r:tt)*) => {
+        let s = $s;
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        use std::io::Read;
+        let mut s = String::new();
+        std::io::stdin().read_to_string(&mut s).unwrap();
+        parse!{s => $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+    ($iter:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn sub() -> bool {
+    parse! {
+        _n: usize,
+        s: String,
+    }
+    let find_i = s.find('I');
+    if find_i.is_none() {
+        return false;
+    }
+    let find_k = s.rfind('I');
+    if find_k.is_none() {
+        return false;
+    }
+    let i = find_i.unwrap();
+    let k = find_k.unwrap();
+    if i == k {
+        return false;
+    }
+    let find_j = s[i + 1..k].find('O');
+    return find_j.is_some();
+}
+
+fn main() {
+    let ans = if sub() { "Yes" } else { "No" };
+    println!("{}", ans);
+}
